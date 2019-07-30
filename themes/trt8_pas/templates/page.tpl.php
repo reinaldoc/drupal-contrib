8c71057d9887ad0dd09cd44a563c7fdba7ddf605
Marcos Antonio Dias Dos Santos <marcosdiazs1995@gmail.com>
2019-07-30 11:46:47 -0300

    atualizacao css geral

--- themes/trt8_pas/templates/page.tpl.php
+++ themes/trt8_pas/templates/page.tpl.php
@@ -250,12 +250,12 @@
           <?php print render($page['footer']) ?>
         </div>
       <?php endif; ?>
-      
+      <!--
       <div id="copyright">
         <p class="copyright"><?php print t('Copyright'); ?> &copy; <?php echo date("Y"); ?>, <?php print $site_name; ?> </p> <p class="credits"> <?php print t('Designed by'); ?>  <a href="http://www.zymphonies.com">Zymphonies</a></p>
         <div class="clear"></div>
       </div>
-
+      --> <!-- removendo copyright -->
     </div>
  
   </div>
